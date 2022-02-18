3a8a5e9e529d36cddc24fcb398a296cc7584a142
verilye <connor.j.logan@outlook.com>
2022-02-18 12:00:10 +1000

    DTOs and authentication added

---
+++ API/DTOs/LoginDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class LoginDto
+    {
+        [Required]
+        public string UserName{get;set;}
+        public string PassWord{get;set;}
+    }
+}
