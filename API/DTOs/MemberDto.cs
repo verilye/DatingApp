51890450929d5f2ea71930bde3bdc2b8e041cc32
verilye <connor.j.logan@outlook.com>
2022-03-09 08:21:29 +1000

    Applied the repository pattern over the EF core DataContext at the behest of the tutorial man. Unsure if appropriate for commercial environment

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -2,20 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using API.Extensions;
 
-namespace API.Entities
+namespace API.DTOs
 {
-    public class AppUser
+    public class MemberDto
     {
-        public int Id { get; set;}
-        public string UserName { get; set; }
+        
+         public int Id { get; set;}
+        public string Username { get; set; }
 
-        public byte[] PasswordHash {get; set;}
-
-        public byte[] PasswordSalt{get;set;}
-
-        public DateTime DateOfBirth {get;set;}
+        public string PhotoUrl{get;set;}
+        public int Age {get;set;}
 
         public string KnownAs{get;set;}
 
@@ -35,12 +32,8 @@ namespace API.Entities
 
         public string Country{get;set;}
 
-        public ICollection<Photo> Photos {get;set;}
+        public ICollection<PhotoDto> Photos {get;set;}
 
-        public int GetAge(){
 
-            return DateOfBirth.CalculateAge();
-
-        }
     }
 }
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -18,6 +19,8 @@ namespace API.Extensions
         {
 
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
             {
 
