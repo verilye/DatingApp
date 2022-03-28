e611d079316a84a8d171562ef5bd49a1eceaf12d
verilye <connor.j.logan@outlook.com>
2022-03-28 23:55:49 +1000

    Added the like feature and populated the liked user list tab

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -37,10 +37,8 @@ namespace API.Entities
 
         public ICollection<Photo> Photos {get;set;}
 
-        // public int GetAge(){
+        public ICollection<UserLike> LikedUsers {get;set;}
 
-        //     return DateOfBirth.CalculateAge();
-
-        // }
+        public ICollection<UserLike> LikedByUsers {get;set;}
     }
 }
